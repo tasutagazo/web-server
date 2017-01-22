f53a1b149955b24f5e33e5487135a0a1820f0074
Toby Paul <tasutagazo@gmail.com>
2017-01-22 15:03:27 +0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		var date = new Date().toString();
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' time: ' + date);
+		next();
+	}
+}
+app.use(middleware.logger);
+// req = request made by the user 
+// res = what we want to send back
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('Najib is the best bitch in the world');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log("Using Port " + PORT);
+});
